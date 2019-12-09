0bfb1042dedbd01b7d7f927afb02078ccea0aa3d
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2019-12-09 21:33:25 +0700

    spawner berbentuk kursa log tapi pergerakan ke kanan

--- build/Spawner.js
+++ build/Spawner.js
@@ -72,6 +72,7 @@ class musuh {
     }
 }
 
+
 class Minions extends musuh {
     constructor(x, y, speed) {
         super(6, 12, speed);
@@ -82,6 +83,15 @@ class Minions extends musuh {
     shootPeebles() {
 
     }
+}
+
+class MinionsUptoDown extends musuh {
+    constructor(x, y, speed) {
+        super(6, 12, speed);
+        this._object = Spawn(x, y);
+        enemy.push(this);
+        scene.add(this._object);
+    }
     move() {
         if (this.object.position.y >= 2)
             this.object.translateY(-1 * this.speed);
@@ -89,10 +99,12 @@ class Minions extends musuh {
 }
 
 
-class Minions5xPower extends musuh {
-    constructor(x, y, speed) {
-        super(1, 12, speed);
+class MinionsxPower extends musuh {
+    constructor(x, y, speed, translation, range) {
+        super(6, 12, speed);
         this._object = Spawn(x, y);
+        this.translation = translation;
+        this.range = range
         enemy.push(this);
         scene.add(this._object);
     }
@@ -100,10 +112,57 @@ class Minions5xPower extends musuh {
         var coordinate = this.object.position.x - this.speed
             //x = 10y^1/2
         this.object.position.x = coordinate;
-        this.object.position.y = Math.pow(coordinate, 2) / 10
+        this.object.position.y = Math.pow(coordinate - this.translation, 2) / this.range
     }
 }
 
+
+class MinionsxLogLeft extends musuh {
+    constructor(head, x, lebar, speed, translation, accelerate) {
+        super(6, 12, speed);
+        this._object = Spawn(x, 10);
+        this.translation = translation;
+        this.head = head
+        this.lebar = lebar
+        this._accelerate = accelerate;
+        enemy.push(this);
+        scene.add(this._object);
+    }
+    move() {
+        var coordinate = this.object.position.x + this.speed
+            //y = head * log(x + translation)
+        this.object.position.x = coordinate;
+        this.object.position.y = this.head * Math.log2((this.lebar * coordinate) - this.translation) + 10
+        this.accelerates()
+    }
+    accelerates() {
+        this._speed += this._accelerate;
+    }
+}
+class MinionsxLogRight extends musuh {
+    constructor(head, x, lebar, speed, translation, accelerate) {
+        super(6, 12, speed);
+        this._object = Spawn(x, 10);
+        this.translation = translation;
+        this.head = head
+        this.lebar = lebar
+        this._accelerate = accelerate;
+        enemy.push(this);
+        scene.add(this._object);
+    }
+    move() {
+        var coordinate = this.object.position.x + this.speed
+            //y = head * log(x + translation)
+        this.object.position.x = coordinate;
+        this.object.position.y = this.head * Math.log2((this.lebar * coordinate) - this.translation) + 10
+        this.accelerates()
+    }
+    accelerates() {
+        this._speed += this._accelerate;
+    }
+}
+
+//new MinionsxLog(-11, 0, 10, 0.0005, -3, 0.0001) //head (-1 dari atas ke bawah),x,y,speed,translation,akselerasi
 class Boss extends musuh {
     constructor(x, y, speed) {
         super(100, 120, speed);
@@ -154,9 +213,12 @@ function getRandom() {
 // }
 
 
-function enemySpawner() {
+function enemySpawner() { //test object
     if (time <= 3) {
-        new Minions(getRandom() % 10, 10, 0.03)
-        new Minions5xPower(10, 10, 0.08)
+        // new MinionsUptoDown(getRandom() % 10, 10, 0.03) // x , y, speed
+        // new MinionsxPower(25, 10, 0.08, -10, 50)
+        // new MinionsxPower(-25, 10, -0.08, 15, 50) //x coordinate, y coordinate, speed, x translation, Lebar lingkaran
+        new MinionsxLogLeft(-3, -6, 1, 0.05, -6, 0.0005)
+            //head (-1 dari atas ke bawah),x,lebar,speed,translation,akselerasi
     }
 }
