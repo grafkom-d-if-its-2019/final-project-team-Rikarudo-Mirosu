67ca8e9ce60111a35e52504a0397b2d2a1eb9ceb
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2019-12-10 08:27:37 +0700

    sekarang bisa nambah lagu toho sangat epik

---
+++ build/music.js
@@ -0,0 +1,11 @@
+  // create a global audio source
+  function loadMusic() {
+
+      // load a sound and set it as the Audio object's buffer
+      var audioLoader = new THREE.AudioLoader();
+      audioLoader.load('music/th06_15.wav', function(buffer) {
+          sound.setBuffer(buffer);
+          sound.setLoop(true);
+          sound.setVolume(0.5);
+      });
+  }
