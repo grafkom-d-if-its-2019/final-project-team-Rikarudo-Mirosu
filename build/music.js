2ee468514e3bd59569da0184e89c24c43c5e0b4e
csrausyan <43034118+csrausyan@users.noreply.github.com>
2019-12-16 06:14:50 +0700

    meh

--- build/music.js
+++ build/music.js
@@ -3,7 +3,7 @@
 
       // load a sound and set it as the Audio object's buffer
       var audioLoader = new THREE.AudioLoader();
-      audioLoader.load('music/Vivat ITS.mps', function(buffer) {
+      audioLoader.load('music/Vivat ITS.mp3', function(buffer) {
             sound.setBuffer(buffer);
             sound.setLoop(true);
             sound.setVolume(0.5);
